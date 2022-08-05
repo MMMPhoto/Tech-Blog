6ffe146b3c006b8964e1acaa457b6bd34902c433
Max McDonough <max.mcdonough@gmail.com>
2022-08-05 15:53:34 -0400

    Set up database, sezuelize, connection file

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+import Sequelize from "sequelize";
+import 'dotenv/config'
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+
+export default sequelize;
---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import session from 'express-session';
+import exphbs from 'express-handlebars';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// import routes from './controllers/index.js';
+import sequelize from './config/connection.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: false,
+  };
+  
+  app.use(session(sess));
+  
+//   const hbs = exphbs.create({ helpers });
+  
+//   app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+// Express middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+//   app.use(routes);
+  
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+  });
