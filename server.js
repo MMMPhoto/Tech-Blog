811dbe8668c99f66660501e4fd094777085bc9be
Max McDonough <max.mcdonough@gmail.com>
2022-08-23 12:47:43 -0400

    Basic handlebars setup

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ import exphbs from 'express-handlebars';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Seet up Sequelize
 import router from './controllers/index.js';
 import sequelize from './config/connection.js';
 
+// Set up express and set port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,22 +19,21 @@ const sess = {
     secret: 'Super secret secret',
     resave: false,
     saveUninitialized: false,
-  };
-  
-  app.use(session(sess));
-  
-  const hbs = exphbs.create({});
-  
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
+};
+app.use(session(sess));
+
+// Set up handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
   
 // Express middleware
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
   
-  app.use(router);
+app.use(router);
   
-  sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
-  });
+});
